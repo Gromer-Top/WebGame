0033faee278a7bdd129b0bb03adb3cb5b690c539
Gromer <rus.solovey.09@mail.ru>
2025-01-02 16:31:21 +0300

    update game.js

--- game.js
+++ game.js
@@ -1,13 +1,20 @@
+"use strict"
 const canvas = document.getElementById('gameCanvas');
-const context = canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
 
 let x = 0;
+let speed = 10;
 
 function gameLoop() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.fillStyle = 'red';
-    context.fillRect(x, 100, 50, 50);
-    x += 10;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.beginPath();
+    ctx.arc(x, canvas.height/2, 50, 0, 2*Math.PI, false) ;
+    ctx.fillStyle = 'red';
+    ctx.fill();
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = 'red';
+    ctx.stroke();
+    x += speed;
     requestAnimationFrame(gameLoop);
     if (x > canvas.width+50) x=-100
 }
