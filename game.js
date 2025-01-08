19ce412dfa3e683ea34cc10107e7be5f68d10e82
Gromer <rus.solovey.09@mail.ru>
2025-01-08 15:37:32 +0300

    update

--- game.js
+++ game.js
@@ -2,26 +2,26 @@
 const canvas = document.getElementById('gameCanvas');
 const ctx = canvas.getContext('2d');
  
-// Статы игрока(шарика)
+// Статы игрока(шарика)-->Позиция и физические характеристики<--
 //Позиция
 let y = canvas.height / 2;
 let x = canvas.width - 72;
 
 //Физические характеристики
-let directionX = 0;
-let directionY = 0;
 let speedY = 0;
 let speedX = 0;
 let radius = 20;
 let speedMax = 10;
+let gravity = 10;
+let jumpForce = -2;
 let ax = 0.1;
 let axMax = 2;
 let ay = 1;
 
 //Кнопки
-function left() {directionX = -1;ax += 0.7;}
-function right(){directionX = 1;ax += 0.7;}
-function jump() {}
+function left() {ax -= 0.7;}
+function right(){ax += 0.7;}
+function jump() {speedY=0;ay = jumpForce;}
 function brake() {}
 
 //Звуки
@@ -30,59 +30,72 @@ function playSoundCollision(){
   soundCollision.play();
 }
 
+//Проверка столкновений и воспроизведение звука при столкновении
 function detectCollision(){
   if(x <= 0 + radius)
   {
     playSoundCollision();
-    directionX = 1;
-    ax -= ax * 0.6;
-    speedX -= speedX * 0.8;
-    x = radius;
+    ax = -(ax - ax * 0.6);
+    speedX = -(speedX - speedX * 0.4);
+    x = radius + 1;
   }
+  
   if (x >= canvas.width - radius)
   {
     playSoundCollision();
-    directionX = -1;
-    ax -= ax * 0.6;
-    speedX -= speedX * 0.8;
-    x = canvas.width - radius;
+    ax = -(ax - ax * 0.6);
+    speedX = -(speedX - speedX * 0.4);
+    x = canvas.width - radius - 1;
   }
+  
   if (y <= 0 + radius)
   {
-    directionY = 0;
-    y = 0 + radius;
+    y = radius;
   }
+  
   if (y >= canvas.height - radius)
   {
-    directionY = 0;
     y = canvas.height-radius;
   }
 }
 
+//Движение
 function move(){
   if(ax>axMax){ax=axMax;}
+  if(ax<-axMax){ax=-axMax;}
   speedX += ax;
-  if(speedX > speedMax){speedX=speedMax;}
   speedY += ay;
-  x += speedX * directionX;
+  if(speedX > speedMax){speedX=speedMax;}
+  if(speedX < -speedMax){speedX=-speedMax;}
+  if(speedY > gravity){speedY = gravity;}
+  if(speedY > gravity){speedY = gravity;}
+  if(speedY < -5){speedY = -5;}
+  x += speedX;
   y += speedY;
   detectCollision();
   if(speedX.toFixed()== 0){speedX = 0;}
   if(speedX > 0){speedX-=0.2;}
   if(speedX < 0){speedX+=0.2;}
+  if(speedY < 10) {speedY += 0.2;}
+  if(ax.toFixed()== 0){ax = 0;}
   if(ax > 0){ax-= 0.03;}
-  if(ax < 0){ax=0;}
+  if(ax < 0){ax+= 0.03;}
+
+  if(ay < gravity){ay += 0.05;}
 }
 
+//Отрисовка статы(для разработчиков)
 function drawStats(){
   ctx.fillStyle = "black";
   ctx.font = "bold 20pt Arial";
-  ctx.fillText("Speed: " + speedX.toFixed(2), 10, 25);
-    ctx.fillText("Ax: " + ax.toFixed(2), 200, 25);
-    ctx.fillText("DX: " + directionX.toFixed(2), 390, 25);
-    ctx.fillText("x: " + x.toFixed() + " y: " + y.toFixed(), 10, 50);
+  ctx.fillText("SPEED-X: " + speedX.toFixed(2), 10, 25);
+  ctx.fillText("AX: " + ax.toFixed(2), 220, 25);
+  ctx.fillText("X: " + x.toFixed() + " Y: " + y.toFixed(), 10, 75);
+  ctx.fillText("SPEED-Y: " + speedY.toFixed(2), 10, 50);
+  ctx.fillText("AY: " + ay.toFixed(2), 220, 50);
 }
 
+//Отрисовываем игрока
 function drawPlayer(){
   ctx.beginPath();
   ctx.arc(x, y, radius, 0, 2*Math.PI, false) ;
@@ -94,6 +107,7 @@ function drawPlayer(){
   move();    
 }
 
+//Главная функция
 function main() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawStats();
