742e50f9f996bf0a51643528d717f5c0883a47e2
Gromer <rus.solovey.09@mail.ru>
2025-01-12 16:55:19 +0300

    update game.js

--- game.js
+++ game.js
@@ -129,7 +129,7 @@ function move(){
   if(speed[0] > 0){a[0] -= 0.2;}
   if(speed[0] < 0){a[0] += 0.2;}
   if(a[0].toFixed() == 0 && speed[0].toFixed() == 0){a[0] = 0;speed[0] = 0;}
-  if(a[1] < speedMax[1] && !isGround){a[1] += 0.1;}
+  if(a[1] < speedMax[1] && !isGround){a[1] += speedMax[1] / 100;}
   if(isGround){a[1] = 0;speed[1] = 0;}
 }
 
